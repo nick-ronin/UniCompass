b420b81ba1bcfb4ae7c6a8903850985ac7d083a8
ник ронин <zlatarud@mail.ru>
2025-10-30 01:54:49 +0700

    jfdsdnjk

--- UniCompass/settings.py
+++ UniCompass/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = []
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
