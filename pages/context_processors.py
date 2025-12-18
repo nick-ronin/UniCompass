d5613db633e595033964336bff849b4fd6435d27
nicky fisher <zlatarud@mail.ru>
2025-12-18 13:23:25 +0700

    Delete pages directory

--- pages/context_processors.py
+++
@@ -1,8 +0,0 @@
-from .forms import LanguageForm
-from django.utils import translation
-
-def language_form(request):
-    return {
-        'language_form': LanguageForm(initial={'language': translation.get_language()}),
-        'current_language': translation.get_language(),
-    }
